7eb64140cabdb4fda5772314c9a638cbb222a1de
Roberta Ferreira Aguilar <roberta.aguilar@kobel.com.br>
2023-05-17 20:40:24 -0300

    bg-web

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -3,7 +3,7 @@ import { Text, View } from 'react-native'
 
 export default function App() {
   return (
-    <View className="bg-gray-950 flex-1 justify-center items-center">
+    <View className='bg-gray-950 flex-1 justify-center items-center'>
       <Text className="text-gray-50 font-bold text-5xl">Hello World!</Text>
       <StatusBar style="auto" translucent/>
     </View>
